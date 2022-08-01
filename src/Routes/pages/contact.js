9c75f8db73f57995b4d8f8f58fac137584b147a4
krintsii <fetranomenaa@gmail.com>
2022-08-01 10:42:49 +0300

    modification et finalisation

--- src/Routes/pages/contact.js
+++ src/Routes/pages/contact.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
         res.status(200).render('contact')
         
     })
+
+    // cette methode est pour que n'utilise pas de methde fetch 
     // app.post('/contact', (req, res)=> {
     //     const data = req.body
     //     connection.query("INSERT INTO Notice SET ?;", data, function (err, result, fields) {
